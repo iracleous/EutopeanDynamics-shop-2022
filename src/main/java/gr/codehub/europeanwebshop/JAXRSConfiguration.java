01217ad51ea1c631313327b69c96baa747eb9d68
dimitris <iracleous@gmail.com>
2022-12-23 09:32:52 +0200

    initial commit

---
+++ src/main/java/gr/codehub/europeanwebshop/JAXRSConfiguration.java
@@ -0,0 +1,15 @@
+package gr.codehub.europeanwebshop;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+ 
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
